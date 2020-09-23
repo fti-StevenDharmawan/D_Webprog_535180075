218cddc1b1214d8ca54b6fdbcf8c8f98e9147392
fti-StevenDharmawan <steven.535180075@stu.untar.ac.id>
2020-09-23 18:10:32 +0700

    add

---
+++ Week 4/script.js
@@ -0,0 +1,6 @@
+function changeLine1Style(){
+    document.getElementById("cLine1").className="MakeRed";
+}
+function changeStyle(id,className){
+    document.getElementById(id).className=className;
+}
